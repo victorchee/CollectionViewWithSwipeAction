55376cb87ca028fe0835cb2687d611f883ac2bc1
Victor Chee <victor.qhj@gmail.com>
2019-01-07 14:36:01 +0800

    Add project.

---
+++ CollectionViewWithSwipeAction/CollectionViewWithSwipeAction/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectionViewWithSwipeAction
+//
+//  Created by Migu on 2019/1/7.
+//  Copyright © 2019 VIctorChee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
